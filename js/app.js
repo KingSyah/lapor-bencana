306ae0f870f9bf515e22a5e98a5495ad47529c70
KingSyah <syah.syw@gmail.com>
2026-04-20 13:18:56 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/js/sw.js')
+      .register('js/sw.js')
       .then((reg) => console.info('[PWA] Service Worker registered:', reg.scope))
       .catch((err) => console.warn('[PWA] Service Worker registration failed:', err));
   });
